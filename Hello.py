3d0577852494d0d50f951ced76d31ef6a7372ca3
colby-reyes <colby.n.reyes@gmail.com>
2023-12-29 16:41:11 +0000

    move form element outside cache

--- Hello.py
+++ Hello.py
@@ -41,31 +41,31 @@ def get_sharepoint_spreadsheets():
         #     st.session_state.df_list = [df1, df2]
         return df1,df2
 
+def login_form():
+    """Form with widgets to collect user information"""
+    with st.form("Login"):
+        st.text_input("Username", key="username")
+        st.text_input("Password", type="password", key="password")
+        st.form_submit_button("Log in", on_click=password_entered)
+
+def password_entered():
+    """Checks whether a password entered by the user is correct."""
+    if st.session_state["username"] in st.secrets[
+        "passwords"
+    ] and hmac.compare_digest(
+        st.session_state["password"],
+        st.secrets.passwords[st.session_state["username"]],
+    ):
+        st.session_state["password_correct"] = True
+        del st.session_state["password"]  # Don't store the username or password.
+        del st.session_state["username"]
+    else:
+        st.session_state["password_correct"] = False
 
 @st.cache_resource()
 def check_password():
     """Returns `True` if the user had a correct password."""
 
-    def login_form():
-        """Form with widgets to collect user information"""
-        with st.form("Login"):
-            st.text_input("Username", key="username")
-            st.text_input("Password", type="password", key="password")
-            st.form_submit_button("Log in", on_click=password_entered)
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["username"] in st.secrets[
-            "passwords"
-        ] and hmac.compare_digest(
-            st.session_state["password"],
-            st.secrets.passwords[st.session_state["username"]],
-        ):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the username or password.
-            del st.session_state["username"]
-        else:
-            st.session_state["password_correct"] = False
 
     # Return True if the username + password is validated.
     if st.session_state.get("password_correct", False):
